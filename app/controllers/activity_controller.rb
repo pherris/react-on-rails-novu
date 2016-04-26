880060c31abfb8dbe25ad8b4ba5974117ddd8ab1
Andrew Ralston <andrew.ralston@novu.com>
2016-04-26 16:26:21 -0500

    initial commit

---
+++ app/controllers/activity_controller.rb
@@ -0,0 +1,15 @@
+class ActivityController < ApplicationController
+  include Strava
+
+  before_filter :configure_strava
+
+  def index
+    render json: @client.list_friends_activities
+  end
+
+  private
+
+  def configure_strava
+    @client ||= Strava::Api::V3::Client.new(:access_token => ENV["STRAVA_SECRET"]) #Ralston's personal token - don't abuse please!
+  end
+end
